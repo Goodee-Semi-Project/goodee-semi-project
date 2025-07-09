106671c5934961ee4ae8c622ad9891953b3cbfc4
Rayne <pluvstream@gmail.com>
2025-07-09 14:44:58 +0900

    Update: Account와 AccountDetail 분리 + mapper 수정

---
+++ Goodee-Semi/src/main/java/com/goodee/semi/dto/AccountDetail.java
@@ -0,0 +1,23 @@
+package com.goodee.semi.dto;
+
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AccountDetail extends Account {
+	private String reg_date;
+	private char gender;
+	private String birth;
+	private String phone;
+	private String email;
+	private int postNum;
+	private String address;
+	private String addressDetail;
+}
