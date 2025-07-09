8dd8ded8896f9670dc00f8b8b93db32db83f091c
RealNixC-C <john7622@naver.com>
2025-07-09 10:30:47 +0900

    tracking check

---
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/QnaBoardListServlet.java
@@ -0,0 +1,27 @@
+package com.goodee.semi.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/qnaBoard/list")
+public class QnaBoardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public QnaBoardListServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/question/questionList.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
