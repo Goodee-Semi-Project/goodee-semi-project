60883a97f2bf1702933e38b71ddeae0fba45c1ef
Rayne <pluvstream@gmail.com>
2025-07-09 15:44:42 +0900

    Add: 회원가입 기능 추가

--- Goodee-Semi/src/main/java/com/goodee/semi/dao/AccountDao.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/dao/AccountDao.java
@@ -1,5 +1,21 @@
 package com.goodee.semi.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.goodee.semi.dto.AccountDetail;
+
 public class AccountDao {
+
+	public int insertAccount(SqlSession session, AccountDetail account) {
+		int result = session.insert("com.goodee.semi.mapper.AccountMapper.insertAccount", account);
+		
+		return result;
+	}
+
+	public int insertAccountInfo(SqlSession session, AccountDetail account) {
+		int result = session.insert("com.goodee.semi.mapper.AccountMapper.insertAccountInfo", account);
+		
+		return result;
+	}
 
 }
