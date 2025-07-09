f2bcb599cc30e436abfa811b4ab3dbd1d7c6ecc5
SJ <ligerssj@naver.com>
2025-07-09 19:29:15 +0900

    WIP: 비밀번호 변경 / 암호화 필터 통일화 필요

--- Goodee-Semi/src/main/java/com/goodee/semi/controller/MyInfoEditPw.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/MyInfoEditPw.java
@@ -2,26 +2,23 @@ package com.goodee.semi.controller;
 
 import java.io.IOException;
 
-import com.goodee.semi.dto.Account;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class MyInfoInactiveServlet
+ * Servlet implementation class MyInfoEditPw
  */
-@WebServlet("/myInfo/inactive")
-public class MyInfoInactiveServlet extends HttpServlet {
+@WebServlet("/myInfo/editPw")
+public class MyInfoEditPw extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyInfoInactiveServlet() {
+    public MyInfoEditPw() {
         super();
     }
 
@@ -29,24 +26,13 @@ public class MyInfoInactiveServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: 나의 반려견 리스트
-		
-		request.getRequestDispatcher("/WEB-INF/views/myInfo/myInfoInactive.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: 비밀번호 확인을 세션으로?
 		request.setCharacterEncoding("UTF-8");
-
-		String checkPw = request.getParameter("checkPw");
-		
-		HttpSession session = request.getSession();
-		Account account = (Account) session.getAttribute("loginAccount");
-		String accountId = account.getAccountId();
-		
 	}
 
 }
