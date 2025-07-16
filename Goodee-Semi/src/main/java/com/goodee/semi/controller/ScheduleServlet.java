fe6d223b897fd02c20d0cf20b884574b6402f4c6
passing7by <jinam0717@gmail.com>
2025-07-16 14:52:19 +0900

    Update: 훈련사와 회원의 일정표 조회 페이지&js 구분

--- Goodee-Semi/src/main/java/com/goodee/semi/controller/ScheduleServlet.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/ScheduleServlet.java
@@ -37,12 +37,15 @@ public class ScheduleServlet extends HttpServlet {
 		param.setAccountNo(accountDetail.getAccountNo());
 		
 		// 3. 표시할 회원 정보 바인딩
-		String authurName = (accountDetail.getAuthor() == 1) ? "훈련사" : "회원";
+		int authorNo = accountDetail.getAuthor();
+		String authurName = (authorNo == 1) ? "훈련사" : "회원";
 		String regDate = accountDetail.getReg_date().split(" ")[0].replace("-", ".");
 		request.setAttribute("authurName", authurName);
 		request.setAttribute("regDate", regDate);
 		
-		request.getRequestDispatcher("/WEB-INF/views/schedule/schedule.jsp").forward(request, response);
+		// 훈련사와 회원은 서로 다른 페이지로 이동
+		if(authorNo == 1) request.getRequestDispatcher("/WEB-INF/views/schedule/schedule_trainer.jsp").forward(request, response);
+		else request.getRequestDispatcher("/WEB-INF/views/schedule/schedule_member.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		request.getRequestDispatcher("/WEB-INF/views/schedule/schedule_trainer.jsp").forward(request, response);
+	}
+}
+
