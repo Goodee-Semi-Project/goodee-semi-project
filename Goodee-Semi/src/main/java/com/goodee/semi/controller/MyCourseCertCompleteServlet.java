ee25c7422f672e00a9b268ecc60dd3e9ae41c00e
Coma <comasocean@gmail.com>
2025-07-17 20:20:18 +0900

    Add: 수료증 출력 구현

---
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/MyCourseCertCompleteServlet.java
@@ -0,0 +1,61 @@
+package com.goodee.semi.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.goodee.semi.dto.AccountDetail;
+import com.goodee.semi.dto.Course;
+import com.goodee.semi.dto.Pet;
+import com.goodee.semi.service.ClassService;
+import com.goodee.semi.service.CourseService;
+import com.goodee.semi.service.PetService;
+
+@WebServlet("/myCourse/certComplete")
+public class MyCourseCertCompleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private CourseService courseService = new CourseService();
+	private PetService petService = new PetService();
+	private ClassService classService = new ClassService();
+       
+  public MyCourseCertCompleteServlet() {
+    super();
+  }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		AccountDetail account = (AccountDetail) session.getAttribute("loginAccount");
+		
+		List<Course> courseList = courseService.selectMyCourse(account);
+		List<Course> myCourseList = new ArrayList<Course>();
+		
+		for (Course course : courseList) {
+			List<Pet> myPetInCourse = petService.selectMyPetInCourse(course, account);
+			
+			for (Pet pet : myPetInCourse) {
+				Course courseByPet = courseService.selectCourseOne(String.valueOf(course.getCourseNo()));
+				courseByPet.setMyPetInCourse(pet);
+				courseByPet.setClassData(classService.selectClassByCourseNoAndPetNo(courseByPet.getCourseNo(), pet.getPetNo()));
+				
+				if (courseByPet.getClassData().getClassProg() == 100) {
+					myCourseList.add(courseByPet);
+				}
+			}
+		}
+		
+		request.setAttribute("courseList", myCourseList);
+		request.getRequestDispatcher("/WEB-INF/views/myCourse/certComplete.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
