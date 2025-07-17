c30274c4b914efdfc288dc11b8b0230867ce6cba
RealNixC-C <john7622@naver.com>
2025-07-17 18:01:22 +0900

    WIP : 출석관리 현황 진행중

--- Goodee-Semi/src/main/java/com/goodee/semi/controller/AttendServlet.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/AttendServlet.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.goodee.semi.dto.Account;
 import com.goodee.semi.dto.Course;
+import com.goodee.semi.dto.Pet;
 import com.goodee.semi.service.CourseService;
 
 import jakarta.servlet.ServletException;
@@ -27,9 +28,21 @@ public class AttendServlet extends HttpServlet {
 		HttpSession session = request.getSession();
 		
 		Account account = (Account)session.getAttribute("loginAccount");
-		int accountNo = account.getAccountNo();
+		int accountNo = 0;
+		int author = 0;
 		
-		List<Course> courseList = courseService.selectAllCourseByAccountNo(accountNo);
+		if(account != null) {
+			 accountNo = account.getAccountNo();
+			 author = account.getAuthor();
+		}
+		
+		List<Course> courseList = null;
+		
+		switch (author) {
+		case 1: courseList = courseService.selectAllCourseByAccountNo(accountNo); break;
+		case 2: courseList = null;
+			break;
+		}
 		
 		request.setAttribute("courseList", courseList);
 		request.getRequestDispatcher("/WEB-INF/views/attend/attend.jsp").forward(request, response);
