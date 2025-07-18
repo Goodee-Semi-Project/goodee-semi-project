c11d9af9f549b5226441eb0ee085b158aba3d22a
passing7by <jinam0717@gmail.com>
2025-07-18 12:09:44 +0900

    Refactor: 일정표 기능 관련 클래스명 'event~'에서 'schedule~'로 변경 / Fix: 일정 등록이 되지 않는 문제 해결

---
+++ Goodee-Semi/src/main/java/com/goodee/semi/mapper/ScheduleMapper.java
@@ -0,0 +1,18 @@
+package com.goodee.semi.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.goodee.semi.dto.Schedule;
+
+public interface ScheduleMapper {
+
+	List<Schedule> selectScheduleList(Map<String, String> map);
+	List<Schedule> selectCourseList(int accountNo);
+	List<Schedule> selectAccountList(int courseNo);
+	List<Schedule> selectPetList(Map<String, Integer> map);
+	int selectClassNo(Schedule sched);
+	int insert(Schedule sched);
+	Schedule selectSchedule(int schedNo);
+	int update(Schedule sched);
+}
