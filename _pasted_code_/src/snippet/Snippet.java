88e70852978bae45ef7db5af8054021dee172d67
passing7by <jinam0717@gmail.com>
2025-07-18 12:10:31 +0900

    Delete _pasted_code_/src/snippet directory

--- _pasted_code_/src/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		request.getRequestDispatcher("/WEB-INF/views/schedule/schedule_trainer.jsp").forward(request, response);
-	}
-}
-
