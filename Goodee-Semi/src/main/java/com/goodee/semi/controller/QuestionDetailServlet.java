c4937e12f8904cb83898e390d72f33b1c4f3e644
RealNixC-C <john7622@naver.com>
2025-07-21 01:28:50 +0900

    WIP : 질문게시판 전체 CSS 작업중

--- Goodee-Semi/src/main/java/com/goodee/semi/controller/QuestionDetailServlet.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/QuestionDetailServlet.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import com.goodee.semi.dto.Answer;
 import com.goodee.semi.dto.Question;
+import com.goodee.semi.service.AccountService;
 import com.goodee.semi.service.QuestionService;
 
 import jakarta.servlet.ServletException;
@@ -16,7 +17,8 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/qnaBoard/detail")
 public class QuestionDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    QuestionService service = new QuestionService();
+    QuestionService questionService = new QuestionService();
+    AccountService accountService = new AccountService();
 	
     public QuestionDetailServlet() {
         super();
@@ -25,9 +27,12 @@ public class QuestionDetailServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		int questNo = Integer.parseInt(request.getParameter("no"));
-		Map<String, Object> result= service.selectDetail(questNo);
+		Map<String, Object> result= questionService.selectDetail(questNo);
 		Question question = (Question)result.get("question");
 		Answer answer= (Answer)result.get("answer");
+		if(answer != null) {
+			answer.setProfileAttach(accountService.selectAttachByAccountNo(answer.getAccountNo()));
+		}
 		
 		request.setAttribute("question", question);
 		request.setAttribute("answer", answer);
