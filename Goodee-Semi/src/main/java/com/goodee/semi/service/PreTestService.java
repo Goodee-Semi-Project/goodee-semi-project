c0d29f5853018f069bc1e462ce69395371368ecf
SJ <ligerssj@naver.com>
2025-07-21 12:57:50 +0900

    WIP: 테스트 없는 사전학습에 테스트 추가

--- Goodee-Semi/src/main/java/com/goodee/semi/service/PreTestService.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/service/PreTestService.java
@@ -2,6 +2,9 @@ package com.goodee.semi.service;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.goodee.semi.common.sql.SqlSessionTemplate;
 import com.goodee.semi.dao.PreTestDao;
 import com.goodee.semi.dto.PreTest;
 
@@ -13,10 +16,33 @@ public class PreTestService {
 	}
 
 	public int updateList(List<PreTest> list) {
+		SqlSession session = SqlSessionTemplate.getSqlSession(false);
 		int result = -1;
-		for (PreTest preTest : list) {
-			result = -1;
-			result = dao.update(preTest);
+		
+		try {
+			for (PreTest preTest : list) {
+				result = -1;
+				
+				result = dao.countOne(session, preTest);
+				
+				if (result > 0) {
+					result = dao.update(session, preTest);
+				} else {
+					result = dao.insertPreTest(session, preTest);
+				}
+				
+			}
+			
+			if (result > 0) {
+				session.commit();
+			} else {
+				session.rollback();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.rollback();
+		} finally {
+			session.close();
 		}
 		
 		return result;
