f52da80efbfe20caad87075dfbcf13d3b0eec24c
passing7by <jinam0717@gmail.com>
2025-07-24 14:51:04 +0900

    Update: 일정 수정 기능 - 예외 처리 수정

--- Goodee-Semi/src/main/java/com/goodee/semi/controller/ScheduleListServlet.java
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/ScheduleListServlet.java
@@ -23,7 +23,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/schedule/list")
 public class ScheduleListServlet extends HttpServlet {
