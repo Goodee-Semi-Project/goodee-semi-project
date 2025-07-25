ee7bc3714cdbea27c4919bd839d9458dd9062e51
Coma <comasocean@gmail.com>
2025-07-25 10:44:22 +0900

    Add: 잘못된 경로로 접근 시 차단 후 홈으로 안내하는 페이지 추가

---
+++ Goodee-Semi/src/main/java/com/goodee/semi/controller/WarningServlet.java
@@ -0,0 +1,26 @@
+package com.goodee.semi.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/invalidAccess")
+public class WarningServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  public WarningServlet() {
+    super();
+  }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/info/invalidAccess.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
